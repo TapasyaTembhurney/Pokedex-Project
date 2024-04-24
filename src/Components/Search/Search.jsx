de3c32d5d184eff999879c954ed1940e975b02d7
TapasyaTembhurney <tapasyatembhurney@gmail.com>
2024-04-24 17:27:44 +0530

    Search modified

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
 function Search() {
   return (
     <div className='search-bar'>
       <input id='pokemon-search' type="text" placeholder='pokemon name'/>
+
     </div>
   )
 }
